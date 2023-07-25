dedbec204d570da16690682792ddc9a6a6544ec1
idob2 <idob@moveo.co.il>
2023-07-25 09:28:29 +0300

    added env variables

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ import dbConnect from './db/db_connect';
 import bodyParser from 'body-parser';
 import chefRouter from "./routes/chef_router";
 import RestaurantRouter from './routes/restaurant_router';
-
+require('dotenv').config();
 const app = express();
 const router = express.Router();
 
@@ -14,8 +14,6 @@ app.use(bodyParser.urlencoded({ extended: true })); // Parse URL-encoded data
 app.use(express.json()); // Parse JSON data in the request body
 app.use("/api/chefs", chefRouter);
 app.use("/api/restaurants", RestaurantRouter);
-
-
 
 app.get('/', (req:any, res:any) => {
   res.send("This is the Epicure server");
